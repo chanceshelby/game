671bea89d5b933f8ccec50d2a474efa8c66a3afd
Chance Shelby <chanceshelby17@gmail.com>
2019-02-17 11:09:59 -0600

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,8 @@
+ function enemy(enemyType, health, mana, strength, agility, speed) {
+	this.enemyType = enemyType;
+	this.health = health;
+	this.mana = mana;
+	this.strenght = strength;
+	this.agility = agility;
+	this.speed = speed;
+}
---
+++ player.js
@@ -0,0 +1,95 @@
+let player;
+
+function Player(classType, health, mana, strength, agility, speed) {
+	this.classType = classType;
+	this.health = health;
+	this.mana = mana;
+	this.strenght = strength;
+	this.agility = agility;
+	this.speed = speed;
+}
+
+let PlayerMoves = {
+	calcAttack: function() {
+		//who attacks first
+		let getPlayerSpeed = player.speed;
+		let get EnemySpeed = enemy.speed;
+
+		function Player(classType, health, mana, strength, agility, speed) {
+			this.classType = classType;
+			this.health = health;
+			this.mana = mana;
+			this.strenght = strength;
+			this.agility = agility;
+			this.speed = speed;
+		}
+
+		let PlayerMoves = {
+			calcAttack: function() {
+				//who attacks first
+				let getPlayerSpeed = player.speed;
+				let get EnemySpeed = enemy.speed;
+		
+				//player attacks
+				let playerAttack = function() {
+					let calcBaseDamage;
+					if (player.mana > 0) {
+						calcBaseDamage = player.strength * player.mana / 1000;
+					} else {
+						calcBaseDamage = player.strength * player.agility / 1000;
+					}
+					let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+					let calcOutputDamage = calcBaseDamage + offsetDamage;
+					//Number of hits
+					let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10) / 2) + 1;
+					let attackValues = [calcOutputDamage, numberOFHits];
+					return attackValues;
+				}
+			}
+				//enemy attacks
+				let enemyAttack = function() {
+					let calcBaseDamage;
+					if (enemy.mana > 0) {
+						calcBaseDamage = enemy.strength * enemy.mana / 1000;
+					} else {
+						calcBaseDamage = enemy.strength * enemy.agility / 1000;
+					}
+					let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+					let calcOutputDamage = calcBaseDamage + offsetDamage;
+					//Number of hits
+					let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10) / 2) + 1;
+					let attackValues = [calcOutputDamage, numberOFHits];
+					return attackValues;
+				}
+				//player/enemy health to change later
+				let getPlayerHealth = document.querySelector(".healt-player");
+				let getEnemyHealth = document.querySelector(".healt-enemy");
+				//initiate attacks
+				if (getPlayerSpeed > getEnemySpeed) {
+					let playerAttackValues = playerAttack();
+					let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+					enemy.health = enemy.health - totalDamage;
+					//alert
+					alert("You hit " + playerAttackValues[0] + " damage " + playerAttackValues[1] + " times");
+					if (enemy.health <= 0) {
+						alert("You win!");
+						getPlayerHealth.innerHTML = 'Health: ' + player.health;
+						getEnemyHealth.innerHTML = 'Health: 0';
+					} else {
+						getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+						//enemy attacks
+						let enemyAttackValues = enemyAttack();
+						let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+						player.health = player.health - totalDamage;
+						//alert
+						alert("Enemy hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times");
+						if (palyer.health <= 0) {
+							alert("You loose!");
+							getPlayerHealth.innerHTML = 'Health: 0';
+							getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+						} else {
+							getPlayerHealth.innerHTML = 'Health: ' + player.health;
+						}
+					}
+				}
+			}
